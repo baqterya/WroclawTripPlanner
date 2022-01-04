ac37e6dbd571e17021a9cdf2fc042513cc871cc2
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2022-01-04 14:18:02 +0100

    Overridden top toolbar

--- WroclawTripPlanner/app/src/main/java/com/baqterya/wroclawtripplanner/view/activity/MainActivity.kt
+++ WroclawTripPlanner/app/src/main/java/com/baqterya/wroclawtripplanner/view/activity/MainActivity.kt
@@ -3,10 +3,19 @@ package com.baqterya.wroclawtripplanner.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.baqterya.wroclawtripplanner.R
+import com.baqterya.wroclawtripplanner.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: MaterialToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toolbar = binding.topToolbar
+        setSupportActionBar(toolbar)
     }
 }
