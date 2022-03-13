1a8d1a032eead1373fdf13ef796c3f2cf9b0e590
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2022-03-13 19:40:57 +0100

    Model classes documented

--- WroclawTripPlanner/app/src/main/java/com/baqterya/wroclawtripplanner/model/Tag.kt
+++ WroclawTripPlanner/app/src/main/java/com/baqterya/wroclawtripplanner/model/Tag.kt
@@ -1,5 +1,11 @@
 package com.baqterya.wroclawtripplanner.model
 
+/**
+ * Data class of a tag that a user can designate for a Place.
+ *
+ * @property tagCounter: counts how many times a tag was added to a place
+ * @property tagAddedByUserIds: list of IDs of users that added the same tag
+ */
 data class Tag(
     var tagName: String? = null,
     var tagCounter: Int = 0,
